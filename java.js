6500da684fdf888ad14c34715591f89fdc780bef
jeet7745 <122025142+jeet7745@users.noreply.github.com>
2023-02-15 11:03:56 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+
+let btn1= document.getElementById("btn1");
+let btn2= document.getElementById("btn2");
+let img= document.getElementById("img");
+function abc(){
+    img.src="bulbon.jpg";
+
+
+}
+
+function xyz(){
+    img.src="bulboff.jpg";
+    
+    
+}
